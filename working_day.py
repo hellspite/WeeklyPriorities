accc2fdc16ddfdb97cd9952fce65708137661885
Emiliano Nobis <emiliano.nobis@gmail.com>
2022-05-10 08:21:48 +0200

    New WorkingDay class

---
+++ working_day.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+
+class WorkingDay:
+    def __init__(self, workingday):
+        self.date = workingday
+        self.orders = []
+
+    def print_day(self):
+        pass
+
+    def add_orders(self):
+        pass
